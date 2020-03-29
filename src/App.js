9713c717370da689464604677d426d379708a310
pbohora <pradeepbohora26@gmail.com>
2020-03-29 17:20:45 +0300

    add useResourse hook for http request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-  
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
-const useField = (type) => {
+const useField = type => {
   const [value, setValue] = useState('')
 
-  const onChange = (event) => {
+  const onChange = event => {
     setValue(event.target.value)
   }
 
@@ -17,22 +15,25 @@ const useField = (type) => {
   }
 }
 
-const useResource = (baseUrl) => {
+const useResource = baseUrl => {
   const [resources, setResources] = useState([])
 
   // ...
+  useEffect(() => {
+    axios.get(baseUrl).then(response => setResources(response.data))
+  }, [])
 
-  const create = (resource) => {
+  const create = async resource => {
     // ...
+    const response = await axios.post(baseUrl, resource)
+    return response.data
   }
 
   const service = {
     create
   }
 
-  return [
-    resources, service
-  ]
+  return [resources, service]
 }
 
 const App = () => {
@@ -43,14 +44,14 @@ const App = () => {
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
-  const handleNoteSubmit = (event) => {
+  const handleNoteSubmit = event => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
-  const handlePersonSubmit = (event) => {
+
+  const handlePersonSubmit = event => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -60,15 +61,21 @@ const App = () => {
         <input {...content} />
         <button>create</button>
       </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map(n => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map(n => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
   )
 }
